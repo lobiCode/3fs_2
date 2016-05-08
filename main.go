36a3bbb7a4b817de419758ecc5749e9daa95ad40
Uros Slana <urossla@gmail.com>
2016-05-09 01:08:32 +0200

    added  Reverse text and Text encoder resolver

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"net"
+	_ "strconv"
+	_ "strings"
 )
 
 const (
@@ -9,7 +12,12 @@ const (
 	BUFFER = 100
 )
 
-var JobQueue chan Job
+var (
+	JobQueue  chan Job
+	FIBONACCI = []byte("Fibonacci")
+	REVERSE   = []byte("ReverseText")
+	ENCODER   = []byte("TextEncoder")
+)
 
 func main() {
 
@@ -27,12 +35,13 @@ func main() {
 			continue
 
 		}
-		go dispatchers(conn)
+		dispatchers(conn)
 	}
 
 }
 
 func Init() {
+
 	JobQueue = make(chan Job, BUFFER)
 
 	for i := 0; i < N; i++ {
@@ -42,6 +51,28 @@ func Init() {
 }
 
 func dispatchers(conn net.Conn) {
-	j := Job{conn, new(Fibonacci)}
+
+	var j Job
+	b := make([]byte, 512)
+	conn.Read(b) // TODO len n,
+
+	req := bytes.Split(b, []byte{'\n'})
+	req = bytes.Split(req[0], []byte{' '})
+	// TODO len(req) < 2  close, return
+
+	switch {
+	case bytes.Compare(req[0], FIBONACCI) == 0:
+		j = Job{conn, Fibonacci{req[1]}}
+	case bytes.Compare(req[0], REVERSE) == 0:
+		j = Job{conn, ReverseText{req[1]}}
+	case bytes.Compare(req[0], ENCODER) == 0:
+		j = Job{conn, TextEncoder{req[1]}}
+	default:
+		b = []byte("is unkowon resolver")
+		req = append(req, b)
+		conn.Write(bytes.Join(req, []byte{' '}))
+		conn.Close()
+		return
+	}
 	JobQueue <- j
 }
--- qmanager.go
+++ qmanager.go
@@ -1,7 +1,17 @@
 package main
 
 import (
+	_ "encoding/binary"
+	"golang.org/x/crypto/bcrypt"
+	"math"
 	"net"
+	"strconv"
+)
+
+const (
+	R   = 2.236067977499789696409173668731276235440618359611525724270
+	PHI = (1 + R) / 2
+	Phi = (1 - R) / 2
 )
 
 type Resolver interface {
@@ -33,10 +43,53 @@ func (w *Worker) Start() {
 	}()
 }
 
-type Fibonacci int
+type Fibonacci struct {
+	B []byte
+}
 
 func (f Fibonacci) R(conn net.Conn) {
 
-	conn.Write([]byte("Closed\n"))
+	n, err := strconv.Atoi(string(f.B))
+	if err != nil {
+		// TODO err msq, close, return
+	}
+
+	nfloat := float64(n)
+	fib := (math.Pow(PHI, nfloat) - math.Pow(Phi, nfloat)) / R
+	s := strconv.FormatFloat(fib, 'f', 0, 64)
+	s = s + "\n"
+	conn.Write([]byte(s))
 	conn.Close()
+}
+
+type ReverseText struct {
+	B []byte
+}
+
+func (rt ReverseText) R(conn net.Conn) {
+
+	for i := 0; i < len(rt.B)/2; i++ {
+		j := len(rt.B) - 1 - i
+		rt.B[i], rt.B[j] = rt.B[j], rt.B[i]
+	}
+
+	rt.B = append(rt.B, '\n')
+	conn.Write(rt.B)
+	conn.Close()
+}
+
+type TextEncoder struct {
+	B []byte
+}
+
+func (te TextEncoder) R(conn net.Conn) {
+
+	b, err := bcrypt.GenerateFromPassword(te.B, bcrypt.DefaultCost)
+	if err != nil {
+		// TODO
+	}
+
+	conn.Write(b)
+	conn.Close()
+
 }
